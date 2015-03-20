2bd2879121182f4f9293ba9405211309e2796cdd
Zoe Ma <ym7fd@virginia.edu>
2015-03-20 10:00:29 -0400

    adapted to draw additional columns

---
+++ pTyrSignaling_03202015.R
@@ -0,0 +1,29 @@
+ptyr <- read.csv("/Users/yizhuoma/Desktop/312/barcode.csv")
+
+statuslist <- unique(ptyr$Status)
+
+
+titleprefixes <-c("Int_n_Bk_n_A","Int_n_Bk", "Int_n_A_n_Bk")
+blank.frame <-data.frame(Int_n_Bk_n_A = numeric(), Int_n_Bk = numeric(), Int_n_A_n_Bk= numeric(), Sample.ID = numeric(), Gel = numeric())
+
+for (status in statuslist){
+  filled.frame <- blank.frame
+  status.frame <- ptyr[ptyr$Status==status,]
+  
+  for (i in seq(1,8)){ 
+    subtable <- status.frame[,c(paste0(titleprefixes,".G",i),"Sample.ID")]
+    subtable$Gel <- i
+    colnames(subtable) <- colnames(filled.frame)
+    head(subtable)
+    filled.frame <- rbind(filled.frame,subtable)
+  }
+  for (i in seq(1,3)){
+    titleprefix <- titleprefixes[i]
+  pdf(paste0("/Users/yizhuoma/Desktop/312/pTyrSignalling_Boxplots_",status,titleprefix,".pdf"),width=11,height=8, title=paste("pTyr Signalling -",status,titleprefix))
+  plot(filled.frame[,c(titleprefix)] ~ as.factor(filled.frame$Sample.ID),ylab=paste("Normalized Intensity",titleprefix), xlab="Sample ID",main=paste("pTyr Signalling -",status))
+  points(filled.frame[,titleprefix] ~ as.factor(filled.frame$Sample.ID),col=filled.frame$Gel)
+  
+  legend('topright',paste("Gel",c(unique(filled.frame$Gel))),col=unique(filled.frame$Gel),pch=1,cex=.7)
+  dev.off()
+  }
+}
