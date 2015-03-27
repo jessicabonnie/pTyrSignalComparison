bf59d648aaaec8544bf520f74e108107a1d89fb6
JB <jessia.k.bonnie@gmail.com>
2015-03-27 10:59:34 -0400

    added FamilyID column to munged tables

--- pTyrSignaling_03202015.R
+++ pTyrSignaling_03202015.R
@@ -12,13 +12,13 @@ statuslist <- unique(ptyr$Status)
 
 titleprefixes <-c("Int_n_Bk_n_A","Int_n_Bk", "Int_n_A_n_Bk")
 blank.frame <-data.frame(Int_n_Bk_n_A = numeric(), Int_n_Bk = numeric(), Int_n_A_n_Bk= numeric(), 
-                         Sample.ID = numeric(), Status=character(), Gel = numeric())
+                         Sample.ID = numeric(), Family_ID = character(), Status=character(), Gel = numeric())
 whole.frame <- blank.frame
 for (status in statuslist){
   filled.frame <- blank.frame
   status.frame <- ptyr[ptyr$Status==status,]
   for (i in seq(1,8)){ 
-    subtable <- status.frame[,c(paste0(titleprefixes,".G",i),"Sample.ID","Status")]
+    subtable <- status.frame[,c(paste0(titleprefixes,".G",i),"Sample.ID", "Family_ID","Status")]
     subtable$Gel <- i
     colnames(subtable) <- colnames(filled.frame)
     head(subtable)
@@ -27,13 +27,13 @@ for (status in statuslist){
   }
   for (i in seq(1,3)){
     titleprefix <- titleprefixes[i]
-  pdf(paste0(wkdir,"pTyrSignalling_Boxplots_",status,titleprefix,".pdf"),width=11,height=8, title=paste("pTyr Signalling -",status,titleprefix))
+  #pdf(paste0(wkdir,"pTyrSignalling_Boxplots_",status,titleprefix,".pdf"),width=11,height=8, title=paste("pTyr Signalling -",status,titleprefix))
   plot(log(filled.frame[,c(titleprefix)],base=2) ~ as.factor(filled.frame$Sample.ID),
        ylab=paste("log2(Normalized Intensity)",titleprefix), xlab="Sample ID",main=paste("pTyr Signalling -",status))
-  points(log(filled.frame[,titleprefix],base=2) ~ as.factor(filled.frame$Sample.ID),col=filled.frame$Gel)
+  points(log(filled.frame[,titleprefix],base=2) ~ as.factor(filled.frame$Sample.ID),col=filled.frame$Gel,pch=1)
   
   legend('topright',paste("Gel",c(unique(filled.frame$Gel))),col=unique(filled.frame$Gel),pch=1,cex=.7)
-  dev.off()
+  #dev.off()
   }
 }
 
